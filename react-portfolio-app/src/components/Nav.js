a1daab19d956b9ad70c5cd3f29a73c97070dfff0
curriecoder <andrew.j.currie0@gmail.com>
2021-11-06 18:55:26 -0400

    changed nav to flex-end for right side alignment

--- react-portfolio-app/src/components/Nav.js
+++ react-portfolio-app/src/components/Nav.js
@@ -2,29 +2,31 @@ export default function Nav() {
   const linkStyle = { border: "1px black", padding: "5px" };
 
   return (
-    <nav className="main-header-nav">
-      <section
-        style={{
-          display: "flex",
-          fontFamily: "helvetica",
-          flexDirection: "row",
-          alignItems: "flex-start",
-          justifyContent: "flex-start",
-        }}
-      >
-        <div style={linkStyle}>
-          <a href="#">About Me</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Portfolio</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Contact</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Resume</a>
-        </div>
-      </section>
-    </nav>
+    <header className="main-header">
+      <nav className="main-header-nav">
+        <section
+          style={{
+            display: "flex",
+            fontFamily: "helvetica",
+            flexDirection: "row",
+            alignItems: "flex-start",
+            justifyContent: "flex-end",
+          }}
+        >
+          <div style={linkStyle}>
+            <a href="#">About Me</a>
+          </div>
+          <div style={linkStyle}>
+            <a href="#">Portfolio</a>
+          </div>
+          <div style={linkStyle}>
+            <a href="#">Contact</a>
+          </div>
+          <div style={linkStyle}>
+            <a href="#">Resume</a>
+          </div>
+        </section>
+      </nav>
+    </header>
   );
 }
