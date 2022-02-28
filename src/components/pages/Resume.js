fe1500628d79015e240a35654510aff60da2368a
curriecoder <andrew.j.currie0@gmail.com>
2022-02-28 14:18:45 -0500

    updated resume to latest rev, updated contact info, created todos for css, still a problem opening the resume pdf

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,15 +1,12 @@
 import React from "react";
-import resumeImg from "../../assets/images/Resume.png";
 
 export default function Resume() {
   return (
     <div className="container">
       <h1>Resume</h1>
-      <a href="/assets/pdf/Coding-Resume.pdf" target="_blank" rel="noreferrer">
+      <a href="../assets/pdf/technical-resume-rev1.3.pdf" target="_blank" rel="noreferrer">
         Click here to open my resume in a new tab.
       </a>
-      <br />
-      <img src={resumeImg} alt="resume" />
     </div>
   );
 }
