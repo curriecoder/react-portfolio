6c760ea495bcf7d2987258e34c8885167833141e
curriecoder <andrew.j.currie0@gmail.com>
2022-03-02 12:25:14 -0500

    added png for resume and created it as an anchor tag. Still need to fix the footer weirdness etc

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../../assets/pdf/technical-resume-rev1.3.pdf";
+import resume from "../../assets/images/resume.png";
 
 
 export default function Resume() {
@@ -8,7 +8,7 @@ export default function Resume() {
       <h1>Resume/CV</h1>
       <div>
         <a href="https://docs.google.com/document/d/1QHnXt5mxQQKwaYkD0Ssa4CHcOxsQ5gnl5K5B2Ppk4Pc/edit?usp=sharing" target="_blank" rel="noreferrer">
-          Click for resume PDF in new tab.
+          <img src={resume}></img>
         </a>
       </div>
 
