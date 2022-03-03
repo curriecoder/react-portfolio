05303c2616103283a6aaa6a8c82608f20f463cd1
curriecoder <andrew.j.currie0@gmail.com>
2022-03-03 11:08:54 -0500

    working on footer, now it doesn't overlap with anything

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,46 +4,48 @@ import React from "react";
 //X TODO: Add bottom padding to footer
 export default function Footer() {
   return (
-    <div className="footer">
-      <ul className="footer-list">
-        <li className="footer-item">555-555-5555</li>
-        <li className="footer-item">
-          <a
-            href="mailto:andrew.j.currie0@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            andrew.j.currie0@gmail.com
-          </a>
-        </li>
-        <li className="footer-item">
-          <a
-            href="https://github.com/curriecoder"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub Profile
-          </a>
-        </li>
-        <li className="footer-item">
-          <a
-            href="https://www.linkedin.com/in/andrewjcurrie0/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn Profile
-          </a>
-        </li>
-        <li className="footer-item">
-          <a
-            href="https://soundcloud.com/user-956212347"
-            target="_blank"
-            rel="noreferrer"
-          >
-            SoundCloud Profile
-          </a>
-        </li>
-      </ul>
-    </div>
+    <footer className="footer">
+      <div className="container">
+        <ul className="footer-list">
+          <li className="footer-item">555-555-5555</li>
+          <li className="footer-item">
+            <a
+              href="mailto:andrew.j.currie0@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              andrew.j.currie0@gmail.com
+            </a>
+          </li>
+          <li className="footer-item">
+            <a
+              href="https://github.com/curriecoder"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub Profile
+            </a>
+          </li>
+          <li className="footer-item">
+            <a
+              href="https://www.linkedin.com/in/andrewjcurrie0/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn Profile
+            </a>
+          </li>
+          <li className="footer-item">
+            <a
+              href="https://soundcloud.com/user-956212347"
+              target="_blank"
+              rel="noreferrer"
+            >
+              SoundCloud Profile
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
 }
